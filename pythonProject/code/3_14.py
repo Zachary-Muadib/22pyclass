feecedc88e7d74e4b730aa07cfd535c7e6b50484
Zachary-Muadib <chinacs7@icloud.com>
2023-11-02 00:30:47 +0800

    Update files.

--- pythonProject/code/3_14.py
+++ pythonProject/code/3_14.py
@@ -3,9 +3,12 @@ import random
 
 random1 = int(random.randint(0, 100))
 random2 = int(random.randint(0, 100))
+print(f"整数1 = {random1}", end="，")
+print(f"整数2 = {random2}")
 
 # gcd()函数求最大公约数
-gcd = math.gcd(random1)
+gcd = math.gcd(random1, random2)
+print(f"最大公约数 = {gcd}")
 
 # 辗转相除法求最大公约数
 if random1 == random2:
@@ -21,3 +24,22 @@ else:
         m, n = n, m % n
     gcd = n
 print(f"最大公约数 = {gcd}")
+
+# 更相减损法求最大公约数
+m = random1
+n = random2
+while m != n:
+    if m > n:
+        m = m - n
+    else:
+        n = n - m
+gcd = m
+print(f"最大公约数 = {gcd}")
+
+# lcm()函数求最小公倍数
+lcm = math.lcm(random1, random2)
+print(f"最小公倍数 = {lcm}")
+
+# 公式求最小公倍数
+lcm = random1 * random2 // gcd
+print(f"最小公倍数 = {lcm}")
